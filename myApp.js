a1db99b3d72e9ebc4c5abb104b5c99eef24ac681
c4v <60588908+sudo-c4v@users.noreply.github.com>
2025-01-17 01:35:15 -0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require('express', 'helmet'); 
 const app = express();
 app.use(helmet.hidePoweredBy());
 
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
@@ -10,11 +22,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-module.exports = app;
-
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
